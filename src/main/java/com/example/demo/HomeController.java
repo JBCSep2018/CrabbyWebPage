17d6f71613f01c0ffacf9e0c6217acaa6e22c296
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-11-05 13:07:01 -0500

    initial commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -5,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
   @RequestMapping("/")
   public String homePage(){
+
     return "index";
   }
 }
